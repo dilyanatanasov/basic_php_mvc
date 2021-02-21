1fb4f0320c324069670ddabc3b3818abf2bf8858
Dilyan <dilyan.atanasov@scavasoft.com>
2021-02-21 18:47:47 +0200

    basic design

--- views/login.php
+++ views/login.php
@@ -3,12 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="/mvc/views/css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300&display=swap" rel="stylesheet">
 </head>
 <body>
-    <form action="/mvc/index.php" method="post">
-        <input name="username">
-        <input name="password"><br>
-        <button type="submit">Login</button>
+    <form class="container" action="/mvc/index.php" method="post">
+        <img class="logo" src="/mvc/views/img/new_logo.png">
+        <input name="username" placeholder="username">
+        <input name="password" placeholder="password">
+        <input type="submit" value="Login"/>
     </form>
 </body>
 </html>
