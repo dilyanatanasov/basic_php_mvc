6b77a23c9f6dc31d4c617233531495ec882977c2
Dilyan <dilyan.atanasov@scavasoft.com>
2021-04-13 20:47:01 +0300

    fix file name issue

--- core/UploadManager.php
+++ core/UploadManager.php
@@ -3,7 +3,7 @@
 class UploadManager {
     public function uploadImg() {
         $target_dir = "views/img/";
-        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        $target_file = $target_dir . round(microtime(true) * 1000) . "_" . basename($_FILES["file_to_upload"]["name"]);
         $uploadOk = 1;
         $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
@@ -11,7 +11,6 @@ class UploadManager {
         $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
         if($check !== false) {
             echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
         } else {
             echo "File is not an image.";
             $uploadOk = 0;
